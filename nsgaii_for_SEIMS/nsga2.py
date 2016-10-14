1777feb6851d7ccc1e2d4d11df5aff14448b9758
gaohr <gaohrgao@163.com>
2016-10-14 23:21:47 +0800

    update

---
+++ nsgaii_for_SEIMS/nsga2.py
@@ -0,0 +1,176 @@
+'''
+@summary: Implementation of the NSGA-II algorithm in Python.
+@version: 1.0
+@since: 2011-01-07
+@author: Marcelo Pita, http://marcelopita.wordpress.com
+@modified: Huiran Gao
+@date: 2016-10-12
+@contact: marcelo.souza.pita <at> gmail.com
+@copyright: Copyright 2011 Marcelo Pita
+@license:
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import sys, random
+from solution import *
+
+class NSGAII:
+    # Implementation of NSGA-II algorithm.
+    current_evaluated_objective = 0
+
+    def __init__(self, num_objectives, mutation_rate=0.05, crossover_rate=0.65):
+        '''
+        Constructor.
+        @num_objectives: number of objectives,
+        @mutation_rate: mutation rate (default value 5%) and
+        @crossover_rate: crossover rate (default value 65%).
+        '''
+        self.num_objectives = num_objectives
+        self.mutation_rate = mutation_rate
+        self.crossover_rate = crossover_rate
+        random.seed();
+        
+    def run(self, P, population_size, num_generations):
+        '''
+        Run NSGA-II.
+        @P: chromosomes
+        @population_size: number of chromosome
+        @num_generations: iteration number of genetic
+        '''
+        for s in P:
+            s.evaluate_solution()
+        Q = []
+        for i in range(num_generations):
+            print "Iteration: ", i
+            R = []
+            R.extend(P)
+            R.extend(Q)
+            fronts = self.fast_nondominated_sort(R)
+            # print len(fronts[1])
+            del P[:]
+            for front in fronts.values():
+                if len(front) == 0:
+                    break
+                self.crowding_distance_assignment(front);
+                P.extend(front)
+                if len(P) >= population_size:
+                    break
+            
+            self.sort_crowding(P)
+            if len(P) > population_size:
+                del P[population_size:]
+            Q = self.make_new_pop(P)
+
+        for f in fronts.values():
+            for info in f:
+                print info.__dict__['attributes']
+        # print fronts.values()[0][0].__dict__
+        # for ss in range(len(Q)):
+        #     print Q[ss].__dict__['attributes']
+    def sort_ranking(self, P):
+        for i in range(len(P) - 1, -1, -1):
+            for j in range(1, i + 1):
+                s1 = P[j - 1]
+                s2 = P[j]
+                if s1.rank > s2.rank:
+                    P[j - 1] = s2
+                    P[j] = s1
+                    
+    def sort_objective(self, P, obj_idx):
+        for i in range(len(P) - 1, -1, -1):
+            for j in range(1, i + 1):
+                s1 = P[j - 1]
+                s2 = P[j]
+                if s1.objectives[obj_idx] > s2.objectives[obj_idx]:
+                    P[j - 1] = s2
+                    P[j] = s1
+                    
+    def sort_crowding(self, P):
+        for i in range(len(P) - 1, -1, -1):
+            for j in range(1, i + 1):
+                s1 = P[j - 1]
+                s2 = P[j]
+                if crowded_comparison(s1, s2) < 0:
+                    P[j - 1] = s2
+                    P[j] = s1
+                
+    def make_new_pop(self, P):
+        # Make new population Q, offspring of P.
+        Q = []
+        while len(Q) != len(P):
+            selected_solutions = [None, None]
+            while selected_solutions[0] == selected_solutions[1]:
+                for i in range(2):
+                    s1 = random.choice(P)
+                    s2 = s1
+                    while s1 == s2:
+                        s2 = random.choice(P)
+                    if crowded_comparison(s1, s2) > 0:
+                        selected_solutions[i] = s1
+                    else:
+                        selected_solutions[i] = s2
+            
+            if random.random() < self.crossover_rate:
+                child_solution = selected_solutions[0].crossover(selected_solutions[1])
+                if random.random() < self.mutation_rate:
+                    child_solution.mutate()
+                child_solution.evaluate_solution()
+                Q.append(child_solution)
+        return Q
+        
+    def fast_nondominated_sort(self, P):
+        # Discover Pareto fronts in P, based on non-domination criterion.
+        fronts = {}
+        S = {}
+        n = {}
+        for s in P:
+            S[s] = []
+            n[s] = 0
+        fronts[1] = []
+        
+        for p in P:
+            for q in P:
+                if p == q:
+                    continue
+                if p >> q:
+                    S[p].append(q)
+                elif p << q:
+                    n[p] += 1
+            if n[p] == 0:
+                fronts[1].append(p)
+        i = 1
+
+        while len(fronts[i]) != 0:
+            next_front = []
+            for r in fronts[i]:
+                for s in S[r]:
+                    n[s] -= 1
+                    if n[s] == 0:
+                        next_front.append(s)
+            i += 1
+            fronts[i] = next_front
+        return fronts
+        
+    def crowding_distance_assignment(self, front):
+        # Assign a crowding distance for each solution in the front.
+        for p in front:
+            p.distance = 0
+        
+        for obj_index in range(self.num_objectives):
+            self.sort_objective(front, obj_index)
+            front[0].distance = float('inf')
+            front[len(front) - 1].distance = float('inf')
+            for i in range(1, len(front) - 1):
+                front[i].distance += (front[i + 1].distance - front[i - 1].distance)
---
+++ nsgaii_for_SEIMS/nsga2_example.py
@@ -0,0 +1,98 @@
+'''
+@summary: Implementation of the NSGA-II algorithm in Python.
+@version: 1.0
+@since: 2011-01-10
+@author: Marcelo Pita, http://marcelopita.wordpress.com
+@modified: Huiran Gao
+@date: 2016-10-12
+@contact: marcelo.souza.pita <at> gmail.com
+@copyright: Copyright 2011 Marcelo Pita
+@license:
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import random, math
+from solution import Solution
+from nsga2 import NSGAII
+from matplotlib import pyplot as plt
+import time
+
+class T1Solution(Solution):
+    # Solution for the T1 function.
+    def __init__(self, chro_size):
+        # Constructor.
+        Solution.__init__(self, num_objs)
+        # self.min = 0.
+        # self.max = 1.
+        self.num_objectives = num_objs
+        self.size = chro_size
+
+        for _ in range(self.size):
+            self.attributes.append(random.randint(1, 5))
+        self.evaluate_solution()
+        
+    def evaluate_solution(self):
+        # Implementation of method evaluate_solution() for T1 function.
+        self.objectives[0] = sum(self.attributes)
+        self.objectives[1] = -sum([math.pow(abs(x), 0.5)**3 for x in self.attributes])
+        # print self.objectives[0]
+        
+    def crossover(self, other):
+        # Crossover of T1 solutions.
+        child_solution = T1Solution(self.size)
+        cpoint = random.randint(0, self.size - 1)
+        child_solution.attributes.extend(self.attributes[0 : cpoint])
+        child_solution.attributes.extend(other.attributes[cpoint : self.size])
+        return child_solution
+
+    
+    def mutate(self):
+        # Mutation of T1 solution.
+        mpoint = random.randint(0, self.size - 1)
+        self.attributes[mpoint] = random.randint(1, 5)
+
+if __name__ == '__main__':
+
+    pop_size = 50   # size of populations
+    chro_size = 20 # size of chrosome
+    num_gens = 80   # number of generations
+
+    muta_rate=0.05  # mutation rate
+    crsr_rate=0.65  # crossover rate
+    num_objs = 2    # number of objectives
+    startT = time.clock()
+
+    nsga2 = NSGAII(num_objs, muta_rate, crsr_rate)
+    P = []
+    for i in range(pop_size):
+        P.append(T1Solution(chro_size))
+    nsga2.run(P, pop_size, num_gens)
+
+    endT = time.clock()
+    print "Running time: %.2fs" % (endT - startT)
+
+    obj1 = []
+    obj2 = []
+    for i in range(len(P)):
+        obj1.append(P[i].objectives[0])
+        obj2.append(P[i].objectives[1])
+
+    plt.scatter(obj1, obj2, c="b")
+    plt.show()
+
+
+
+
+
+    
